446addbc29fb610729270383cc80d23beb1ef842
dani <dani@dev.(none)>
2012-11-28 17:16:45 +0000

    first commit - adding all files to repo

---
+++ com.example.yamba.StatusActivity/src/com/example/yamba/XMLParser.java
@@ -0,0 +1,129 @@
+package com.example.yamba;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import android.content.Context;
+import android.util.Log;
+
+public class XMLParser {
+	private Context context;
+	
+	public XMLParser()
+	{
+		//
+	}
+	public String getXmlFromUrl(String url) {
+        String xml = null;
+ 
+        try {
+            // defaultHttpClient
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+ 
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            xml = EntityUtils.toString(httpEntity);
+ 
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // return XML
+        return xml;
+    }
+	public String getXmlFromFile(String filepath) {
+        String xml = null;
+        int resId = context.getResources().getIdentifier(filepath, "raw", "com.example.yamba");
+        InputStream is = context.getResources().openRawResource(resId);
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr, 8192); 
+        
+        try {
+            String test;	
+            while (true){				
+                test = br.readLine();   
+                // readLine() returns null if no more lines in the file
+                if(test == null) break;
+                xml.concat(test);
+            }
+            isr.close();
+            is.close();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // return XML
+        return xml;
+    }
+	
+	public Document getDomElement(String xml){
+        Document doc = null;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+ 
+            DocumentBuilder db = dbf.newDocumentBuilder();
+ 
+            InputSource is = new InputSource();
+                is.setCharacterStream(new StringReader(xml));
+                doc = db.parse(is); 
+ 
+            } catch (ParserConfigurationException e) {
+                Log.e("Error: ", e.getMessage());
+                return null;
+            } catch (SAXException e) {
+                Log.e("Error: ", e.getMessage());
+                return null;
+            } catch (IOException e) {
+                Log.e("Error: ", e.getMessage());
+                return null;
+            }
+                // return DOM
+            return doc;
+    }
+    
+    public String getValue(Element item, String str) {
+        NodeList n = item.getElementsByTagName(str);
+        return this.getElementValue(n.item(0));
+    }
+     
+    public final String getElementValue( Node elem ) {
+             Node child;
+             if( elem != null){
+                 if (elem.hasChildNodes()){
+                     for( child = elem.getFirstChild(); child != null; child = child.getNextSibling() ){
+                         if( child.getNodeType() == Node.TEXT_NODE  ){
+                             return child.getNodeValue();
+                         }
+                     }
+                 }
+             }
+             return "";
+    }
+}
